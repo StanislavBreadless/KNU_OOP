33ed2d5937fa275299ab62dc1731a490bbe9abb7
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2020-10-17 23:48:08 +0300

    Actual tests are run

--- smart-sheets/renderer/src/table-manager/FormulaVisitor.ts
+++ smart-sheets/renderer/src/table-manager/FormulaVisitor.ts
@@ -24,43 +24,83 @@ export class FormulaVisitor extends LabCalculatorVisitor<string> {
     super();
   }
 
+  aggregateResult(aggregate: string, nextResult: string) {
+    return (+aggregate + nextResult).toString();
+  }
+
   // @ts-ignore 
   visitIdentifierExpr(ctx: IdentifierExprContext): string {
-    return this.variableResolver(ctx.toString());
+    return this.variableResolver(ctx.text);
+  }
+
+  // @ts-ignore
+  visitExpression(ctx: ExpressionContext): string {
+    return this.visitChildren(ctx);
   }
 
   // @ts-ignore
   visitNumberExpr(ctx: NumberExprContext): string {
-    return ctx.toString();
+    return ctx.text;
   }
 
   // @ts-ignore
   visitAdditiveExpr(ctx: AdditiveExprContext): string {
+    this.visitChildren(ctx);
+
     const leftExpr = ctx.expression(0);
     const rightExpr = ctx.expression(1);
 
     const leftValue = +this.visit(leftExpr);
     const rightValue = +this.visit(rightExpr);
 
+    const add = ctx.ADD();
+    const sub = ctx.SUBTRACT();
+
     if(!isFinite(leftValue) || !isFinite(rightValue)) {
       return WRONG_FORMAT;
-    } else {
+    } else if (add) {
       return (leftValue + rightValue).toString();
+    } else if (sub) {
+      return (leftValue - rightValue).toString();
+    } else {
+      return WRONG_FORMAT;
     }
   }
 
   // @ts-ignore 
   visitMultiplicativeExpr(ctx: MultiplicativeExprContext): string {
+    this.visitChildren(ctx);
+
     const leftExpr = ctx.expression(0);
     const rightExpr = ctx.expression(1);
+
+    const multiply = ctx.MULTIPLY();
+    const divide = ctx.DIVIDE();
+    const div = ctx.DIV();
+    const mod = ctx.MOD();
 
     const leftValue = +this.visit(leftExpr);
     const rightValue = +this.visit(rightExpr);
 
     if(!isFinite(leftValue) || !isFinite(rightValue)) {
       return WRONG_FORMAT;
-    } else {
+    }
+
+    if (multiply) {
       return (leftValue * rightValue).toString();
     }
+    if (divide) {
+      return (leftValue / rightValue).toString();
+    }
+    if (div) {
+      const val1 = +leftValue;
+      const val2 = +rightValue;
+      return Math.floor(val1 / val2).toString();
+    }
+    if (mod) {
+      return (leftValue % rightValue).toString();
+    }
+
+    return WRONG_FORMAT;
   }
 }
--- smart-sheets/renderer/src/tests/FormulaEvaluator.ts
+++ smart-sheets/renderer/src/tests/FormulaEvaluator.ts
@@ -5,17 +5,14 @@ describe('Basic parsing', () => {
   const evaluator = new FormulaEvaluator();
 
   it('Returning one number', () => {
-
     const formula = '123';
 
     expect(
-      evaluator.evaluate(formula, () => ''),
-      '123'
-    );
+      evaluator.evaluate(formula, () => '')
+    ).to.equal('123');
   });
 
   it('Resolving one variable', () => {
-
     const formula = 'a10';
 
     const resolver = (variable: string) => {
@@ -23,51 +20,63 @@ describe('Basic parsing', () => {
     }
 
     expect(
-      evaluator.evaluate(formula, resolver),
-      '123'
-    );
+      evaluator.evaluate(formula, resolver)
+    ).to.be.equal('123');
   });
 
   it('Additive expression', () => {
-
-    const formula = 'a10 + b12 + 5';
+    const formula = 'a10 + b12 - 5';
 
     const resolver = (variable: string) => {
       return variable === 'a10' ? '12' : '32';
     }
 
     expect(
-      evaluator.evaluate(formula, resolver),
-      '49'
-    );
-  });
-
-  it('Additive expression', () => {
-
-    const formula = 'a10 + b12 + 5';
-
-    const resolver = (variable: string) => {
-      return variable === 'a10' ? '12' : '32';
-    }
-
-    expect(
-      evaluator.evaluate(formula, resolver),
-      '49'
-    );
+      evaluator.evaluate(formula, resolver)
+    ).to.be.equal('39');
   });
 
   it('Mutliplicative expression', () => {
+    const values = {
+      "a10": '12',
+      "b10": '134',
+      "c10": '90',
+      "d10": '110'
+    };
 
-    /// 122 * 12 + 12 * 32 + 20 = 1334 + 384 + 20 = 1738
-    const formula = '112 * a10 + a10 * b12 + 5 * 4';
+    /// 12 * 134 + 90 * 5 + 110/3 + 12 div 5 - 90 mod 7 = 
+    /// 1608 + 450 + 36.666... + 2 - 6 = 2090.666667
+    const formula = 'a10 * b10 + c10 * 5 + d10 / 3 + a10 div 5 - c10 mod 7';
 
     const resolver = (variable: string) => {
-      return variable === 'a10' ? '12' : '32';
+      // @ts-ignore
+      return values[variable] as string;
     }
 
     expect(
-      evaluator.evaluate(formula, resolver),
-      '1738'
-    );
+      (+evaluator.evaluate(formula, resolver)).toFixed(6)
+    ).to.be.equal('2090.666667');
+  });
+
+  it('Mutliplicative expression', () => {
+    const values = {
+      "a10": '12',
+      "b10": '134',
+      "c10": '90',
+      "d10": '110'
+    };
+
+    /// 12 * 134 + 90 * 5 + 110/3 + 12 div 5 - 90 mod 7 = 
+    /// 1608 + 450 + 36.666... + 2 - 6 = 2090.666667
+    const formula = 'a10 * b10 + c10 * 5 + d10 / 3 + a10 div 5 - c10 mod 7';
+
+    const resolver = (variable: string) => {
+      // @ts-ignore
+      return values[variable] as string;
+    }
+
+    expect(
+      (+evaluator.evaluate(formula, resolver)).toFixed(6)
+    ).to.be.equal('2090.666667');
   });
 });
